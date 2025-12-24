2db4724f455269f25c93d822d4ea64365d4b7c0d
Sanket Kharat <sanketkharat28@gmail.com>
2025-12-24 14:32:51 +0530

    create resumAI word at top left which when clicked points to homepage

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Navbar = () => {
   return (
-    <div></div>
+    <nav className='navbar'>
+        <Link to='/'>
+        <p className='text-2xl font-bold text-gradient'>ResumAI</p>
+        </Link>
+    </nav>
   )
 }
 
