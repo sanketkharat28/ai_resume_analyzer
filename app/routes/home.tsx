47b326a97d0bf382c4c5065ce5e50cbb27eae934
Sanket Kharat <sanketkharat28@gmail.com>
2026-01-16 23:02:08 +0530

    add puter.js script in root.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,12 +13,13 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
+
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track your Applications and Resume Ratings</h1>
         <h2>Review your submissions and check AI powered feedback</h2>
       </div>
-    </section>
+    
 
     {resumes.length > 0 && (
       <div className="resume-section">
@@ -29,6 +30,6 @@ export default function Home() {
     )
     }
 
-    
+    </section>
   </main>
 }
