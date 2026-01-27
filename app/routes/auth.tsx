3bd57d2028e919e6aef0ddaa36e8bd60d679e416
Sanket Kharat <sanketkharat28@gmail.com>
2026-01-27 14:56:09 +0530

    show signin button if not authenticated else show logout button

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,9 +1,13 @@
+import { usePuterStore } from "~/lib/puter";
+
 export const meta = ()=> ([
 {title : 'ResumAI | Auth'},
 {name : 'description', content:'Log into your account'}
 ])
 
 export default function Auth() {
+  const {isLoading, auth} = usePuterStore();
+
   return (
     <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
         <div className="gradient-border shadow-lg">
@@ -11,6 +15,25 @@ export default function Auth() {
             <div className="flex flex-col items-center gap-2 text-center">
                 <h1>Welcome</h1>
                 <h2>Log in to continue your job journey</h2>
+            </div>
+            <div>
+              {isLoading ? (
+                <button className="auth-button animate-pulse">
+                  Signing You In
+                </button>
+              ) : (
+                <>
+                {auth.isAuthenticated ? (
+                  <button className="auto-button" onClick={auth.signOut}>
+                    Log Out
+                  </button>
+                ) : (
+                  <button className="auto-button" onClick={auth.signIn}>
+                    Log In
+                  </button>
+                )}
+                </>
+              )}
             </div>
         </section>
         </div>
