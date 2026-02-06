20c85ef0bd5c70afcf16bc13b9e0272d27ade85c
Sanket Kharat <sanketkharat28@gmail.com>
2026-02-06 17:33:57 +0530

    fixed issue, name and companyname visible but not the image of the resume

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -32,7 +32,7 @@ const ResumeCard = ({resume:{id, companyName, jobTitle, feedback, imagePath}}:{r
             {resumeUrl && <div className="gradient-border animate-in fade-in duration-1000">
                 <div className="w-full h-full">
                     <img
-                    src={imagePath}
+                    src={resumeUrl}
                     alt="resume"
                     className="w-full h-[350px] max-sm:h-[200px] object-cover obkect-top"
                     />
