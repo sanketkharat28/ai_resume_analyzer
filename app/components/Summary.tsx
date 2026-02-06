7e3185cf6daa6b4e70cf3eff9a3614d471fbffa7
Sanket Kharat <sanketkharat28@gmail.com>
2026-02-06 12:33:31 +0530

    ScoreBadge component

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ScoreGauge from "./ScoreGauge";
+import ScoreBadge from "./ScoreBadge";
 
 const Category = ({ title, score }: { title: string; score: number }) => {
   const textColor =
@@ -14,6 +15,7 @@ const Category = ({ title, score }: { title: string; score: number }) => {
       <div className="category">
         <div className="flex flex-row gap-2 items-center justify-center">
           <p className="text-2xl">{title}</p>
+          <ScoreBadge score={score} />
         </div>
       </div>
       <p className="text-2xl">
